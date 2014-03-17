8a8e58b0454bf848ca0a44821c05b4b91a956ff1
Mudit Malpani <mmalpani@rocketfuel.com>
2014-03-17 01:13:02 -0700

    Store stats
    - Show correct high score, avg score and message on game over page

--- src/com/webtoapp/basetwo/GameActivity.java
+++ src/com/webtoapp/basetwo/GameActivity.java
@@ -1,6 +1,7 @@
 package com.webtoapp.basetwo;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.view.GestureDetectorCompat;
 import android.view.GestureDetector.OnGestureListener;
@@ -17,6 +18,7 @@ import android.widget.TextView;
 public class GameActivity extends Activity implements OnGestureListener {
     private static final int SWIPE_MIN_DISTANCE = 120;
     private static final int SWIPE_THRESHOLD_VELOCITY = 200;
+    public static final String PREFS_NAME = "AppPrefsFile";
     private Board board;
     private GestureDetectorCompat mDetector;
 
@@ -106,13 +108,57 @@ public class GameActivity extends Activity implements OnGestureListener {
         return false;
     }
 
+    private String getGameOverMessage() {
+        String message = "GAME OVER!";
+        int lowerLimit = 21;
+        int upperLimit = 50;
+        if (board.size() == 3) {
+            lowerLimit = 900;
+            upperLimit = 1900;
+        } else if (board.size() == 4) {
+            lowerLimit = 18000;
+            upperLimit = 25000;
+        }
+        if (board.score() > upperLimit) {
+            message = "EXCELLENT!";
+        } else if (board.score() > lowerLimit) {
+            message = "WELL DONE!";
+        }
+        return message;
+    }
+
     public void finishGame() {
+        String message = getGameOverMessage();
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+        int matches = settings.getInt("matches" + board.size(), 0);
+        int highScore = settings.getInt("highScore" + board.size(), 0);
+        int totalScore = settings.getInt("totalScore" + board.size(), 0);
+
+        matches += 1;
+        totalScore += board.score();
+        if (board.score() > highScore) {
+            highScore = board.score();
+            message = "HIGH SCORE!";
+        }
+        int avgScore = matches == 0 ? 0 : totalScore / matches;
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putInt("matches" + board.size(), matches);
+        editor.putInt("highScore" + board.size(), highScore);
+        editor.putInt("totalScore" + board.size(), totalScore);
+        editor.commit();
+
         RelativeLayout resultView = (RelativeLayout) findViewById(R.id.game_over_layout);
         resultView.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
         LayoutInflater.from(getApplicationContext()).inflate(R.layout.game_over, resultView, true);
 
         TextView scoreView = (TextView) findViewById(R.id.score_result_view);
         scoreView.setText(String.valueOf(board.score()));
+        TextView highScoreView = (TextView) findViewById(R.id.high_score_view);
+        highScoreView.setText("High Score: " + String.valueOf(highScore));
+        TextView avgScoreView = (TextView) findViewById(R.id.avg_score_view);
+        avgScoreView.setText("Average Score: " + String.valueOf(avgScore));
+        TextView messageView = (TextView) findViewById(R.id.result_message);
+        messageView.setText(message);
 
         Button restartBtn = (Button) findViewById(R.id.restart_btn);
         restartBtn.setOnClickListener(new View.OnClickListener() {
@@ -142,6 +188,7 @@ public class GameActivity extends Activity implements OnGestureListener {
         startGame();
         displayScore();
     }
+
 
     @Override
     public boolean onDown(MotionEvent e) {
